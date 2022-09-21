42c58a96c9ec3acf5a88f5d9615c13911883531a
Nico <jumpingpxl@gmail.com>
2022-09-21 15:03:18 +0200

    add license

--- core/src/main/java/net/labymod/addons/betterperspective/core/BetterPerspectiveService.java
+++ core/src/main/java/net/labymod/addons/betterperspective/core/BetterPerspectiveService.java
@@ -1,3 +1,19 @@
+/*
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package net.labymod.addons.betterperspective.core;
 
 import com.google.inject.Inject;
@@ -10,46 +26,46 @@ import net.labymod.api.client.options.Perspective;
 @Singleton
 public class BetterPerspectiveService {
 
-    private final LabyAPI labyAPI;
-    private final CameraLockController cameraLockController;
+	private final LabyAPI labyAPI;
+	private final CameraLockController cameraLockController;
 
-    private Perspective previousMode;
-    private boolean active = false;
+	private Perspective previousMode;
+	private boolean active = false;
 
-    @Inject
-    private BetterPerspectiveService(LabyAPI labyAPI, CameraLockController cameraLockController) {
-        this.labyAPI = labyAPI;
-        this.cameraLockController = cameraLockController;
-    }
+	@Inject
+	private BetterPerspectiveService(LabyAPI labyAPI, CameraLockController cameraLockController) {
+		this.labyAPI = labyAPI;
+		this.cameraLockController = cameraLockController;
+	}
 
-    public void toggle() {
-        if (this.active) {
-            this.deactivate();
-        } else {
-            this.activate();
-        }
-    }
+	public void toggle() {
+		if (this.active) {
+			this.deactivate();
+		} else {
+			this.activate();
+		}
+	}
 
-    public void activate() {
-        this.active = true;
+	public void activate() {
+		this.active = true;
 
-        MinecraftOptions options = this.labyAPI.minecraft().options();
-        this.previousMode = options.perspective();
-        options.setPerspective(Perspective.THIRD_PERSON_BACK);
+		MinecraftOptions options = this.labyAPI.minecraft().options();
+		this.previousMode = options.perspective();
+		options.setPerspective(Perspective.THIRD_PERSON_BACK);
 
-        this.cameraLockController.lock(CameraLockController.LockType.HEAD);
-    }
+		this.cameraLockController.lock(CameraLockController.LockType.HEAD);
+	}
 
-    public void deactivate() {
-        this.active = false;
+	public void deactivate() {
+		this.active = false;
 
-        MinecraftOptions options = this.labyAPI.minecraft().options();
-        options.setPerspective(this.previousMode);
+		MinecraftOptions options = this.labyAPI.minecraft().options();
+		options.setPerspective(this.previousMode);
 
-        this.cameraLockController.unlock();
-    }
+		this.cameraLockController.unlock();
+	}
 
-    public boolean isActive() {
-        return this.active;
-    }
+	public boolean isActive() {
+		return this.active;
+	}
 }
--- core/src/main/java/net/labymod/addons/betterperspective/core/listener/KeyListener.java
+++ core/src/main/java/net/labymod/addons/betterperspective/core/listener/KeyListener.java
@@ -1,3 +1,19 @@
+/*
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package net.labymod.addons.betterperspective.core.listener;
 
 import com.google.inject.Inject;
@@ -10,36 +26,36 @@ import net.labymod.api.inject.LabyGuice;
 
 public class KeyListener {
 
-    private final BetterPerspective addon;
+	private final BetterPerspective addon;
 
-    @Inject
-    private KeyListener(BetterPerspective addon) {
-        this.addon = addon;
-    }
+	@Inject
+	private KeyListener(BetterPerspective addon) {
+		this.addon = addon;
+	}
 
-    @Subscribe
-    public void onKey(KeyEvent event) {
-        if (!this.addon.labyAPI().minecraft().isMouseLocked()) {
-            return;
-        }
+	@Subscribe
+	public void onKey(KeyEvent event) {
+		if (!this.addon.labyAPI().minecraft().isMouseLocked()) {
+			return;
+		}
 
-        BetterPerspectiveConfiguration configuration = this.addon.configuration();
-        if (!configuration.enabled().get() || !event.key().equals(configuration.key().get())) {
-            return;
-        }
+		BetterPerspectiveConfiguration configuration = this.addon.configuration();
+		if (!configuration.enabled().get() || !event.key().equals(configuration.key().get())) {
+			return;
+		}
 
-        BetterPerspectiveService service = LabyGuice.getInstance(BetterPerspectiveService.class);
-        if (configuration.toggle().get()) {
-            if (event.state() == KeyEvent.State.UNPRESSED) {
-                service.toggle();
-            }
-            return;
-        }
+		BetterPerspectiveService service = LabyGuice.getInstance(BetterPerspectiveService.class);
+		if (configuration.toggle().get()) {
+			if (event.state() == KeyEvent.State.UNPRESSED) {
+				service.toggle();
+			}
+			return;
+		}
 
-        if (event.state() == KeyEvent.State.UNPRESSED) {
-            service.deactivate();
-        } else if (event.state() == KeyEvent.State.PRESS) {
-            service.activate();
-        }
-    }
+		if (event.state() == KeyEvent.State.UNPRESSED) {
+			service.deactivate();
+		} else if (event.state() == KeyEvent.State.PRESS) {
+			service.activate();
+		}
+	}
 }
