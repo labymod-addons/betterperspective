3e19c20a0f46db709958312caa57746fe725309b
Nico <jumpingpxl@gmail.com>
2023-04-20 16:22:52 +0200

    bump labygradle version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "betterperspective-addon"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.3.7"
+    val labyGradlePluginVersion = "0.3.21"
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }
