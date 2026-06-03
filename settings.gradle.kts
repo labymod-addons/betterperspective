a89d920b48b8aeea96a2c4873237da3a59f573bc
Christian <r0bby@gmx.net>
2026-06-03 13:58:50 +0200

    Update LabyGradle to 0.8.0, sync CI workflow

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "betterperspective-addon"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.5.9"
+    val labyGradlePluginVersion = "0.8.0"
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }
